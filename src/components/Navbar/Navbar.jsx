a29d911e0914a0ce3989168699b2ed30374f8d3c
Adaeze <adaezeaniekee@gmail.com>
2025-03-28 02:51:30 -0700

    made prefect moblie responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 
 import React, { useRef } from "react";
+import { useState } from "react"
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
 import AnchorLink from "react-anchor-link-smooth-scroll";
@@ -12,27 +13,26 @@ import menu_close from "../../assets/xmark-solid.svg"
 const Navbar = () => {
 
   const menuRef = useRef();
+  const [isOpen, setIsOpen] = useState(false);
 
   const openMenu = () => {
     menuRef.current.style.right="0";
+    setIsOpen(true);  // to hide header when menu is open
   }
 
   const closeMenu = () => {
-    menuRef.current.style.right="-350px";
+    menuRef.current.style.right="-200px";
+    setIsOpen(false);  // to show header when menu is closed
   }
+
   
   return (
-    
-    
+   <div className={`navbar${isOpen ? "hide-header":"" }`}>
+      <img className="logoimg" src= {logo} alt=""  />
 
-   <div className="navbar">
-  
-    
-      <img src= {logo} alt=""  />
+      <img src={ menu_open  }  onClick={ openMenu} alt="" width={20} className="nav_open" />
 
-      <img src={ menu_open  } onClick={openMenu} alt="" width={20} className="nav_open" />
-
-      <ul ref={menuRef} className="nav-menu">
+      <ul ref={menuRef} className={`nav-menu ${ isOpen ?"show" : ""}`}>
            
            <img src={ menu_close } onClick={closeMenu} className="nav_close" alt="" width={15} />
 
